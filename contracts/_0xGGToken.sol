ad502800e0e464034a65d9e5050359b54fd36629
wcgate01 <wcgate01@louisville.edu>
2021-01-17 21:32:56 -0500

    add required funtions to smart contract to allow the token to be traded. Added Test Files. Changes contact name

---
+++ contracts/_0xGGToken.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract _0xGGToken {
+    string public name = "0xGG Token";
+    string public symbol = "0xGGT";
+    string public standard = "0xGG Token v1.0ee";
+    uint256 public totalSupply;
+
+    /**
+    Transfer Event
+    */
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    /**
+    Approval Event
+    */
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    /**
+    Mapping
+    */
+    mapping(address => uint256) public balanceOf;
+
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    /**
+    Functions
+    */
+    function oxggToken(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+}
